1cc18845e9146d69f5a397050dde1abdd80e2e78
Karl Lew <karl@firepick.org>
2023-10-03 08:12:49 -0700

    v1.36.238 += update from ebt-site3

--- ebt-config.mjs
+++ ebt-config.mjs
@@ -1,13 +1,16 @@
 const EbtConfig = {
-  "appName": "www.sc-voice.net",
+  "appName": "sc-voice.net",
   "basePath": "/",
   "homePath": "#/wiki/welcome",
   "content": {
     "index": "toc",
   },
+  "footnotes": "Footnotes",
+  "privacyLink": "#/wiki/privacy",
+  "monolingual": false,
   "github": {
-    "account": "ebt-site",
-    "repository": "ebt-site3"
+    "account": "sc-voice",
+    "repository": "sc-voice3"
   }
 }
 
